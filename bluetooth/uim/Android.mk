d3d6d9b170465d5cda345746630b7e4e2d3431d8
Raffaele Aquilone <raquiloX>
2012-12-19 09:41:56 +0100

    Multidevice support: remove build flag and reference to BlueZ
    BZ: 76413
    
    The build flag BOARD_HAVE_TI12XX is no longer used. Furthermore
    BlueZ is not present anymore.
    
    Change-Id: I89bb1288938d4f3d0b68b7f1911d8cbf07153fee
    Signed-off-by: Raffaele Aquilone <raquiloX>
    Reviewed-on: http://android.intel.com:8080/83323
    Reviewed-by: Aquilone, RaffaeleX <raffaelex.aquilone@intel.com>
    Reviewed-by: Gros, Julien <julien.gros@intel.com>
    Tested-by: Champciaux, Nicolas <nicolas.champciaux@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- bluetooth/uim/Android.mk
+++ bluetooth/uim/Android.mk
@@ -1,4 +1,3 @@
-ifeq ($(BOARD_HAVE_TI12XX),true)
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -6,8 +5,7 @@ include $(CLEAR_VARS)
 # UIM Application
 #
 
-LOCAL_C_INCLUDES:= uim.h \
-	external/bluetooth/bluez/
+LOCAL_C_INCLUDES:= uim.h
 
 LOCAL_SRC_FILES:= \
 	uim.c
@@ -16,5 +14,3 @@ LOCAL_SHARED_LIBRARIES:= libnetutils liblog
 LOCAL_MODULE:=uim
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_EXECUTABLE)
-
-endif
